556393faff4c6b45a223733823d25407208afc08
vasi <vasi.che@gmail.com>
2026-03-23 18:09:59 +0500

    delete unused method from EmployeeRepository.java

--- src/main/java/net/guides/springboot2/freemarker/repository/EmployeeRepository.java
+++ src/main/java/net/guides/springboot2/freemarker/repository/EmployeeRepository.java
@@ -11,16 +11,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	@Query("SELECT e FROM Employee e " +
 			"WHERE (:firstName IS NULL OR LOWER(e.firstName) LIKE LOWER(CONCAT('%', :firstName, '%'))) " +
 			"AND (:lastName IS NULL OR LOWER(e.lastName) LIKE LOWER(CONCAT('%', :lastName, '%'))) " +
-			"AND (:email IS NULL OR LOWER(e.email) LIKE LOWER(CONCAT('%', :email, '%')))")
-	Page<Employee> findByFilters(
-			@Param("firstName") String firstName,
-			@Param("lastName") String lastName,
-			@Param("email") String email,
-			Pageable pageable
-	);
-	@Query("SELECT e FROM Employee e " +
-			"WHERE (:firstName IS NULL OR LOWER(e.firstName) LIKE LOWER(CONCAT('%', :firstName, '%'))) " +
-			"AND (:lastName IS NULL OR LOWER(e.lastName) LIKE LOWER(CONCAT('%', :lastName, '%'))) " +
 			"AND (:email IS NULL OR LOWER(e.email) LIKE LOWER(CONCAT('%', :email, '%'))) ")
 	Page<Employee> findByFiltersAndSort(
 			@Param("firstName") String firstName,
