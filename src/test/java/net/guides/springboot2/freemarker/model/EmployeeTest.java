b60a6d8b31a4cb82d7fcbcb767f95aca92b3d5a9
vasi <vasi.che@gmail.com>
2026-03-26 07:52:18 +0500

    Typo

--- src/test/java/net/guides/springboot2/freemarker/model/EmployeeTest.java
+++ src/test/java/net/guides/springboot2/freemarker/model/EmployeeTest.java
@@ -8,30 +8,30 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class EmployeeTest {
 	@Test
 	public void testEquals() {
-		Position position1 = new Position(1L, "Postion 1");
+		Position position1 = new Position(1L, "Position 1");
 
-		Employee employee1 = new Employee(10L, "Firstame", "LastName", "Email", position1);
-		Employee employee2 = new Employee(10L, "Firstame", "LastName", "Email", position1);
+		Employee employee1 = new Employee(10L, "FirstName", "LastName", "Email", position1);
+		Employee employee2 = new Employee(10L, "FirstName", "LastName", "Email", position1);
 
 		assertEquals(employee1, employee2);
 	}
 
 	@Test
 	public void testHashCode() {
-		Position position1 = new Position(1L, "Postion 1");
+		Position position1 = new Position(1L, "Position 1");
 
-		Employee employee1 = new Employee(10L, "Firstame", "LastName", "Email", position1);
-		Employee employee2 = new Employee(10L, "Firstame", "LastName", "Email", position1);
+		Employee employee1 = new Employee(10L, "FirstName", "LastName", "Email", position1);
+		Employee employee2 = new Employee(10L, "FirstName", "LastName", "Email", position1);
 
 		assertEquals(employee1.hashCode(), employee2.hashCode());
 	}
 
 	@Test
 	public void testNotEquals() {
-		Position position1 = new Position(1L, "Postion 1");
+		Position position1 = new Position(1L, "Position 1");
 
-		Employee employee1 = new Employee(10L, "Firstame", "LastName", "Email", position1);
-		Employee employee2 = new Employee(11L, "Firstame", "LastName", "Email", position1);
+		Employee employee1 = new Employee(10L, "FirstName", "LastName", "Email", position1);
+		Employee employee2 = new Employee(11L, "FirstName", "LastName", "Email", position1);
 
 		assertNotEquals(employee1, employee2);
 	}
