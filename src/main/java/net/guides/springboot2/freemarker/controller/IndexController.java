7bbbe512d8ddb0f615548a7854d0d03dd92ded4a
vasi <vasi.che@gmail.com>
2026-04-07 09:22:17 +0500

    добавлена страница по умолчанию

---
+++ src/main/java/net/guides/springboot2/freemarker/controller/IndexController.java
@@ -0,0 +1,12 @@
+package net.guides.springboot2.freemarker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String welcome() {
+		return "/employees/"; // employees — имя представления, которое будет отображаться по умолчанию
+	}
+}
