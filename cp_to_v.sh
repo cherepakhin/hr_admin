1754ec891b5fba79bb531745908fbd7bd7af35d8
vasi <vasi.che@gmail.com>
2026-04-11 12:59:36 +0500

    cp_to_v.sh (ver 5)

--- cp_to_v.sh
+++ cp_to_v.sh
@@ -1,1 +1,1 @@
-scp ~/prog/java/spring-boot/generated_boot_crud/hr-admin/target/hr-admin-0.0.4.jar vasi@v:/home/vasi/temp
+scp ~/prog/java/spring-boot/generated_boot_crud/hr-admin/target/hr-admin-0.0.5.jar vasi@v:/home/vasi/temp
