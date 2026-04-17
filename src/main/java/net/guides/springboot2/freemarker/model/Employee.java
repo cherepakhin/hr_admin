6ae6d14271537b3c2e71bdf4c0bfa37eeb68dd79
vasi <vasi.che@gmail.com>
2026-04-17 11:12:49 +0500

    set size for name of Employee

--- src/main/java/net/guides/springboot2/freemarker/model/Employee.java
+++ src/main/java/net/guides/springboot2/freemarker/model/Employee.java
@@ -1,28 +1,31 @@
 package net.guides.springboot2.freemarker.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Size(min = 3, max = 15, message = "First name must be between 3 to 20 characters long.")
+	private String firstName;
+	private String lastName;
+	private String email;
 	@ManyToOne(fetch = FetchType.EAGER) // EAGER!!! - атрибут нужен всегда, нет смысла его делать lazy
 	@JoinColumn(name = "position_id", nullable = false)
 	private Position position = new Position(-1L, "");
 
-    public Employee() {}
+	public Employee() {
+	}
 
-    public Employee(Long id, String firstName, String lastName, String email, Position position) {
-		this(firstName,lastName, email, position);
+	public Employee(Long id, String firstName, String lastName, String email, Position position) {
+		this(firstName, lastName, email, position);
 		this.id = id;
-    }
+	}
 
 	public Employee(String firstName, String lastName, String email, Position position) {
 		this.firstName = firstName;
@@ -31,18 +34,38 @@ public class Employee {
 		this.position = position;
 	}
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public Position getPosition() {
 		return position;
