b5ac05e87fedc04b606c63d991435a1af4e3fce5
vasi <vasi.che@gmail.com>
2026-08-19 11:36:47 +0500

    //TODO: Криво отображается бутер в свернутом состоянии. Не скрывается надпись.

--- src/main/java/ru/perm/v/hr_admin/controller/IndexController.java
+++ src/main/java/ru/perm/v/hr_admin/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+	//TODO: Криво отображается бутер в свернутом состоянии. Не скрывается надпись.
 	@RequestMapping("/")
 	public String welcome() {
 		return "/employees/"; // employees — имя представления, которое будет отображаться по умолчанию
